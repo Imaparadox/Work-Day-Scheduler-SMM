5354c22eb00b189aa7c9389f68e629d17476efbd
Stephen Marsh <stevemarsh117@gmail.com>
2021-05-17 17:19:25 -0400

    Changed div structure in index.html and made changes to script.js

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # Work-Day-Scheduler-SMM
 
-<!-- GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist -->
+This project is a homework assignment for Rutgers Coding Bootcamp.
+
+## Deploy
+
+[github page site] https://imaparadox.github.io/Work-Day-Scheduler-SMM/
+
+## Description 
+Welcome to the Work-Day-Scheduler! The current date is displayed to assist with creating a schedule for the user. Enter the user events into the time blocks, and click the save button to store the events in the user's inspector local storage. Refreshing the page won't clear the entries in the Application/local storage. The current time will affect each time block's colors, indicating what time slots are available.
+
+
+![ScreenShot](Assets/images/DayScheduler.png "ScreenShot")
+
+## Notes
+N/A
+
+## Licenses
+N/A
+
+## Acknowledgments for advice and assistance:
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,93 @@
-// Current Time
-var currentTime = moment();
-$("#currentDay").text(moment().format("dddd MMMM, YYYY, hA"));
-
-// var presentTime = JSON.stringify(moment().format("dddd MMMM, YYYY"));
-// //append to id="currentDay"
-//  $("#currentDay").append(now);
-//  console.log(presentTime);
-//  console.log(now);
-
-//function for local storage
-function saveTasks() {
-    //When the user clicks the save button the task is stored in local storage
-    //
-}
-
-
-// var inputText = [];
-// $("#container").find("input").each(function () { inputText.push(this.id); });
-
-// for (var i = 0; i > inputText.length; i++) {
-//     var inputCol
-//     $("#i")
-// };
-
-function timeChange() {
-    //if the current time is more than the time block add class of past
-    if (currentTime > 9) {
+//current header date
+var presentDay = moment().format("MMMM Do YYYY hA");
+$("#currentDay").text(presentDay);
+//current date and time
+var timeNow = moment();
+//current hour time
+var presentHour = timeNow.hours();
+//selecting all textarea elements
+var textElements = jQuery("textarea").find("textarea");
+var timeBlocks = $(".time-block");
+//function that stores user inputs
+$(document).ready(function () {
+    timeBlockColor()
+    //local storage
+    $(".saveBtn").on("click", function () {
+        var contentText = $(this).siblings(".description").val();
+        var contentDate = $(this).parent().attr("id");
+        localStorage.setItem(contentText, contentDate);
+    });
+});
+//If statements that show available time-blocks that correspond to the time of day
+function timeBlockColor() {
+    if (presentHour > 8) {
+        $("#hour-8").addClass("past");
+    } else if (presentHour >= 8 && presentHour < 9) {
+        $("#hour-8").addClass("present");
+    } else if (presentHour < 8) {
+        $("#hour-8").addClass("future");
+    };
+    if (presentHour > 9) {
         $("#hour-9").addClass("past");
-        //else the current time is equal to the time block add class of present
-    } else if (currentTime >= 9 && currentTime < 10) {
+    } else if (presentHour >= 9 && presentHour < 10) {
         $("#hour-9").addClass("present");
-        //else the current time is less than the time block add class of future
-    } else if (currentTime < 9) {
+    } else if (presentHour < 9) {
         $("#hour-9").addClass("future");
     };
-    if (currentTime > 10) {
+    if (presentHour > 10) {
         $("#hour-10").addClass("past");
-    } else if (currentTime >= 10 && currentTime < 11) {
+    } else if (presentHour >= 10 && presentHour < 11) {
         $("#hour-10").addClass("present");
-    } else if (currentTime < 10) {
+    } else if (presentHour < 10) {
         $("#hour-10").addClass("future");
     };
-    if (currentTime > 11) {
+    if (presentHour > 11) {
         $("#hour-11").addClass("past");
-    } else if (currentTime >= 11 && currentTime < 12) {
+    } else if (presentHour >= 11 && presentHour < 12) {
         $("#hour-11").addClass("present");
-    } else if (currentTime < 11) {
+    } else if (presentHour < 11) {
         $("#hour-11").addClass("future");
     };
-    if (currentTime > 12) {
+    if (presentHour > 12) {
         $("#hour-12").addClass("past");
-    } else if (currentTime >= 12 && currentTime < 13) {
+    } else if (presentHour >= 12 && presentHour < 13) {
         $("#hour-12").addClass("present");
-    } else if (currentTime < 12) {
+    } else if (presentHour < 12) {
         $("#hour-12").addClass("future");
     };
-    if (currentTime > 13) {
-        $("#hour-13").addClass("past");
-    } else if (currentTime >= 13 && currentTime < 14) {
-        $("#hour-13").addClass("present");
-    } else if (currentTime < 13) {
-        $("#hour-13").addClass("future");
+    if (presentHour > 13) {
+        $("#hour-1").addClass("past");
+    } else if (presentHour >= 13 && presentHour < 14) {
+        $("#hour-1").addClass("present");
+    } else if (presentHour < 13) {
+        $("#hour-1").addClass("future");
     };
-    if (currentTime > 14) {
-        $("#hour-14").addClass("past");
-    } else if (currentTime >= 14 && currentTime < 14) {
-        $("#hour-14").addClass("present");
-    } else if (currentTime < 14) {
-        $("#hour-14").addClass("future");
+    if (presentHour > 14) {
+        $("#hour-2").addClass("past");
+    } else if (presentHour >= 14 && presentHour < 15) {
+        $("#hour-2").addClass("present");
+    } else if (presentHour < 14) {
+        $("#hour-2").addClass("future");
     };
-    if (currentTime > 15) {
-        $("#hour-15").addClass("past");
-    } else if (currentTime >= 15 && currentTime < 16) {
-        $("#hour-15").addClass("present");
-    } else if (currentTime < 15) {
-        $("#hour-15").addClass("future");
+    if (presentHour > 15) {
+        $("#hour-3").addClass("past");
+    } else if (presentHour >= 15 && presentHour < 16) {
+        $("#hour-3").addClass("present");
+    } else if (presentHour < 15) {
+        $("#hour-3").addClass("future");
     };
-    if (currentTime > 16) {
-        $("#hour-16").addClass("past");
-    } else if (currentTime >= 16 && currentTime < 17) {
-        $("#hour-16").addClass("present");
-    } else if (currentTime < 16) {
-        $("#hour-16").addClass("future");
+    if (presentHour > 16) {
+        $("#hour-4").addClass("past");
+    } else if (presentHour >= 16 && presentHour < 17) {
+        $("#hour-4").addClass("present");
+    } else if (presentHour < 16) {
+        $("#hour-4").addClass("future");
     };
-    if (currentTime > 17) {
-        $("#hour-17").addClass("past");
-    } else if (currentTime >= 17 && currentTime < 18) {
-        $("#hour-17").addClass("present");
-    } else if (currentTime < 17) {
-        $("#hour-17").addClass("future");
+    if (presentHour > 17) {
+        $("#hour-5").addClass("past");
+    } else if (presentHour >= 17 && presentHour < 18) {
+        $("#hour-5").addClass("present");
+    } else if (presentHour < 17) {
+        $("#hour-5").addClass("future");
     };
 };
-timeChange();
-
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
