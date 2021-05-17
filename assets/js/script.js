ccaf03a03d86c1c66857617bdb8959020f46cdf8
Stephen Marsh <stevemarsh117@gmail.com>
2021-05-17 16:14:31 -0400

    added if statements to to timeChange function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
 // Current Time
 var currentTime = moment();
 $("#currentDay").text(moment().format("dddd MMMM, YYYY, hA"));
@@ -13,7 +10,8 @@ $("#currentDay").text(moment().format("dddd MMMM, YYYY, hA"));
 
 //function for local storage
 function saveTasks() {
-    var
+    //When the user clicks the save button the task is stored in local storage
+    //
 }
 
 
@@ -25,9 +23,75 @@ function saveTasks() {
 //     $("#i")
 // };
 
-function change() {
-
-}
+function timeChange() {
+    //if the current time is more than the time block add class of past
+    if (currentTime > 9) {
+        $("#hour-9").addClass("past");
+        //else the current time is equal to the time block add class of present
+    } else if (currentTime >= 9 && currentTime < 10) {
+        $("#hour-9").addClass("present");
+        //else the current time is less than the time block add class of future
+    } else if (currentTime < 9) {
+        $("#hour-9").addClass("future");
+    };
+    if (currentTime > 10) {
+        $("#hour-10").addClass("past");
+    } else if (currentTime >= 10 && currentTime < 11) {
+        $("#hour-10").addClass("present");
+    } else if (currentTime < 10) {
+        $("#hour-10").addClass("future");
+    };
+    if (currentTime > 11) {
+        $("#hour-11").addClass("past");
+    } else if (currentTime >= 11 && currentTime < 12) {
+        $("#hour-11").addClass("present");
+    } else if (currentTime < 11) {
+        $("#hour-11").addClass("future");
+    };
+    if (currentTime > 12) {
+        $("#hour-12").addClass("past");
+    } else if (currentTime >= 12 && currentTime < 13) {
+        $("#hour-12").addClass("present");
+    } else if (currentTime < 12) {
+        $("#hour-12").addClass("future");
+    };
+    if (currentTime > 13) {
+        $("#hour-13").addClass("past");
+    } else if (currentTime >= 13 && currentTime < 14) {
+        $("#hour-13").addClass("present");
+    } else if (currentTime < 13) {
+        $("#hour-13").addClass("future");
+    };
+    if (currentTime > 14) {
+        $("#hour-14").addClass("past");
+    } else if (currentTime >= 14 && currentTime < 14) {
+        $("#hour-14").addClass("present");
+    } else if (currentTime < 14) {
+        $("#hour-14").addClass("future");
+    };
+    if (currentTime > 15) {
+        $("#hour-15").addClass("past");
+    } else if (currentTime >= 15 && currentTime < 16) {
+        $("#hour-15").addClass("present");
+    } else if (currentTime < 15) {
+        $("#hour-15").addClass("future");
+    };
+    if (currentTime > 16) {
+        $("#hour-16").addClass("past");
+    } else if (currentTime >= 16 && currentTime < 17) {
+        $("#hour-16").addClass("present");
+    } else if (currentTime < 16) {
+        $("#hour-16").addClass("future");
+    };
+    if (currentTime > 17) {
+        $("#hour-17").addClass("past");
+    } else if (currentTime >= 17 && currentTime < 18) {
+        $("#hour-17").addClass("present");
+    } else if (currentTime < 17) {
+        $("#hour-17").addClass("future");
+    };
+};
+timeChange();
 
 
 // WHEN I view the timeblocks for that day
